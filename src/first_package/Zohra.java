9739e7ed3a378d3949c052760783ebc9156320cd
zohrasulaimany <zohra.sulaimany@gmail.com>
2022-04-07 17:54:30 -0400

    first commint hh

---
+++ src/first_package/Zohra.java
@@ -0,0 +1,30 @@
+package first_package;
+
+public class Zohra {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {
+
+
+                   // lets check
+            //  seconnd\
+
+
+            //dddsddd
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
